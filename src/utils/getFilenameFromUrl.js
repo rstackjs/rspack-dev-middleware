b4c7dfccbf7bc526f52f343622402916ccdd372f
neverland <jait.chen@foxmail.com>
2026-04-15 13:21:10 +0800

    perf: lazily resolve immutable asset info (#38)

--- src/utils/getFilenameFromUrl.js
+++ src/utils/getFilenameFromUrl.js
@@ -99,9 +99,9 @@ function getFilenameFromUrl(context, url) {
   }
 
   for (const {
+    compilation,
     publicPath,
     outputPath,
-    assetsInfo,
     outputFileSystem,
   } of paths) {
     /** @type {string | undefined} */
@@ -144,6 +144,28 @@ function getFilenameFromUrl(context, url) {
         pathname.slice(publicPathPathname.length),
       );
 
+      /** @type {boolean | undefined} */
+      let immutable = undefined;
+
+      /**
+       * @param {FSStats} stats stats
+       * @returns {Extra} extra
+       */
+      const createExtra = (stats) => ({
+        // Lazy evaluate immutable because it may be expensive to get asset info
+        get immutable() {
+          if (immutable === undefined) {
+            const assetName = pathname.slice(publicPathPathname.length);
+            immutable = Boolean(
+              compilation.getAsset(assetName)?.info?.immutable,
+            );
+          }
+          return immutable;
+        },
+        outputFileSystem,
+        stats,
+      });
+
       /**
        * @param {string} filename filename
        * @param {Set<string>=} visited visited filenames
@@ -182,17 +204,10 @@ function getFilenameFromUrl(context, url) {
           return resolveIndex(filename, visited);
         }
 
-        /** @type {Extra} */
-        const extra = {
-          immutable: assetsInfo
-            ? assetsInfo.get(pathname.slice(publicPathPathname.length))
-                ?.immutable
-            : false,
-          outputFileSystem,
-          stats: /** @type {FSStats} */ (stats),
+        return {
+          filename,
+          extra: createExtra(/** @type {FSStats} */ (stats)),
         };
-
-        return { filename, extra };
       };
 
       /**
@@ -221,17 +236,10 @@ function getFilenameFromUrl(context, url) {
           return;
         }
 
-        /** @type {Extra} */
-        const extra = {
-          immutable: assetsInfo
-            ? assetsInfo.get(pathname.slice(publicPathPathname.length))
-                ?.immutable
-            : false,
-          outputFileSystem,
-          stats: /** @type {FSStats} */ (stats),
+        return {
+          filename,
+          extra: createExtra(/** @type {FSStats} */ (stats)),
         };
-
-        return { filename, extra };
       };
 
       if (index.length > 0 && pathname.endsWith("/")) {
--- types/utils/getPaths.d.ts
+++ types/utils/getPaths.d.ts
@@ -1,16 +1,16 @@
 export default getPaths;
 export type Compiler = import("@rspack/core").Compiler;
+export type Compilation = import("@rspack/core").Compilation;
 export type Stats = import("@rspack/core").Stats;
 export type MultiStats = import("@rspack/core").MultiStats;
-export type Asset = import("@rspack/core").Asset;
 export type DevServerOption = import("../index.js").DevServerOption;
 export type IncomingMessage = import("../index.js").IncomingMessage;
 export type OutputFileSystem = import("../index.js").OutputFileSystem;
 export type ServerResponse = import("../index.js").ServerResponse;
 /** @typedef {import("@rspack/core").Compiler} Compiler */
+/** @typedef {import("@rspack/core").Compilation} Compilation */
 /** @typedef {import("@rspack/core").Stats} Stats */
 /** @typedef {import("@rspack/core").MultiStats} MultiStats */
-/** @typedef {import("@rspack/core").Asset} Asset */
 /** @typedef {import("../index.js").DevServerOption} DevServerOption */
 /** @typedef {import("../index.js").IncomingMessage} IncomingMessage */
 /** @typedef {import("../index.js").OutputFileSystem} OutputFileSystem */
@@ -19,7 +19,7 @@ export type ServerResponse = import("../index.js").ServerResponse;
  * @template {IncomingMessage} Request
  * @template {ServerResponse} Response
  * @param {import("../index.js").FilledContext<Request, Response>} context context
- * @returns {{ outputPath: string, outputFileSystem: OutputFileSystem, publicPath: string, assetsInfo: Map<string, Asset["info"]> | undefined }[]} paths
+ * @returns {{ compilation: Compilation, outputPath: string, outputFileSystem: OutputFileSystem, publicPath: string }[]} paths
  */
 declare function getPaths<
   Request extends IncomingMessage,
@@ -27,8 +27,8 @@ declare function getPaths<
 >(
   context: import("../index.js").FilledContext<Request, Response>,
 ): {
+  compilation: Compilation;
   outputPath: string;
   outputFileSystem: OutputFileSystem;
   publicPath: string;
-  assetsInfo: Map<string, Asset["info"]> | undefined;
 }[];
