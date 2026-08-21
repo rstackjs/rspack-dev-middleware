a2f5e4f8562752c01613c6d8ddbd49e9310e8080
Jiahan Chen <jait.chen@foxmail.com>
2026-08-21 16:10:26 +0800

    chore(deps): upgrade Rslint to v0.8.1 (#62)

--- rslint.config.ts
+++ rslint.config.ts
@@ -1,11 +1,20 @@
-import { defineConfig, globalIgnores, ts } from "@rslint/core";
+import { defineConfig, globalIgnores, js, ts } from "@rslint/core";
 
 export default defineConfig([
   globalIgnores(["test/fixtures/broken.js"]),
+  js.configs.recommended,
   ts.configs.recommended,
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
+      "no-undef": "off",
+    },
+  },
+  {
+    files: ["src/**/*.js"],
+    rules: {
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
     },
   },
 ]);
